5b74a3fb5f00f0af4b402b50df977071a4404528
Glenn Della Rocca <glenn@dellarocca.mx>
2024-03-05 14:23:07 -0700

    use the goto function when pressing Button component

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,19 @@
 <script>
+	import { goto } from '$app/navigation';
+	import { Button } from '$components';
+
 	let name = 'Glenn';
 </script>
 
 <h1>About {name}</h1>
+
+<Button
+	on:click={() => {
+		goto('/');
+	}}
+>
+	Go to Home
+</Button>
 
 <style>
 	h1 {
