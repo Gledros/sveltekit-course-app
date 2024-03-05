16d0225d079695bdb08513685a679532e85c5683
Glenn Della Rocca <glenn@dellarocca.mx>
2024-03-05 10:39:18 -0700

    add basic pages for testing the client-side router

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	let name = 'Glenn';
+</script>
+
+<h1>About {name}</h1>
+
+<style>
+	h1 {
+		color: green;
+	}
+</style>
---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+
+	$: console.log($page);
+</script>
+
+<h1>Product page</h1>
+
+<p>Product ID: {$page.params.id}</p>
+
+<a href={`/product/3434`}>Related product</a>
