70084b9ff4094f248765b4f1609140e2054fe890
Glenn Della Rocca <glenn@dellarocca.mx>
2024-03-12 16:26:55 -0700

    feat: move settings pages to admin custom layout group

--- src/routes/(admin)/settings/+layout.svelte
+++ src/routes/(admin)/settings/+layout.svelte
@@ -7,6 +7,7 @@
 	in:slide={{ delay: 600, duration: 300, easing: circInOut }}
 	out:slide={{ delay: 0, duration: 300, easing: circInOut }}
 >
+	<a id="home" href="/">Home</a>
 	Settings:
 	<a href="/settings">Main</a>
 	<a href="/settings/general">General</a>
@@ -18,15 +19,20 @@
 <style>
 	nav {
 		font-size: small;
-		background-color: coral;
+		background-color: rgb(59, 174, 224);
 		font-weight: bold;
 		padding: 1rem;
 		display: flex;
 		gap: 1rem;
-		border-radius: 0 0 0.75rem 0.75rem;
+		border-bottom: 1px solid rgb(98, 98, 98);
 	}
 
 	a {
 		text-decoration: none;
+
+		&#home {
+			margin-left: 1rem;
+			margin-right: 2rem;
+		}
 	}
 </style>
