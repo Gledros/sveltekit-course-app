91eaae808c58b51d3eb7551b9d25162867bdd81f
Glenn Della Rocca <glenn@dellarocca.mx>
2024-03-14 12:25:57 -0700

    feat: add simple navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,31 @@
 	import '../app.css';
 </script>
 
-<slot />
+<nav>
+	<a href="/">Home</a>
+	<a href="/products">Products</a>
+</nav>
+
+<div>
+	<slot />
+</div>
 
 <style lang="scss">
+	nav {
+		padding: 1rem;
+		background-color: rgb(173, 173, 173);
+		color: white;
+		font-weight: bold;
+		border-bottom: 1px solid black;
+		display: flex;
+		gap: 1rem;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	div {
+		padding: 0 2rem;
+	}
 </style>
