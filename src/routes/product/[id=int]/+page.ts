e5ecffaed16627a2568a26d1237c4e79c87385bf
Glenn Della Rocca <glenn@dellarocca.mx>
2024-04-19 18:17:05 -0700

    [feat] add default 404 page by throwing error
    It appears when the product ID doesn't exists.

--- src/routes/product/[id=int]/+page.ts
+++ src/routes/product/[id=int]/+page.ts
@@ -1,6 +1,10 @@
+import { error } from '@sveltejs/kit';
+
 export const load = async ({ params }) => {
 	const products = await (await import('../../products/dummy-products.json')).default;
 	const product = products.products.find((element) => element.id === +params.id);
+
+	if (!product) throw error(404, 'Product not found!');
 
 	return product;
 };
