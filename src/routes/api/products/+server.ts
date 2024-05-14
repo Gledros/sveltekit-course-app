9c264a18bc2d6c9d792e6f5afc3760b67c8ce120
Glenn Della Rocca <glenn@dellarocca.mx>
2024-05-13 18:04:32 -0700

    [feat] implement POST (dummy) endpoint for creating new product

--- src/routes/api/products/+server.ts
+++ src/routes/api/products/+server.ts
@@ -1,8 +1,17 @@
-import { json } from '@sveltejs/kit';
+import { randomUUID as uuid } from 'crypto';
+import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
 	const products = await (await import('../../products/dummy-products.json')).default;
 
 	return json(products);
+};
+
+export const POST: RequestHandler = async ({ request }) => {
+	const product = await request.json();
+
+	if (!product.title) throw error(400, 'Product title is required!');
+
+	return json({ id: uuid(), ...product });
 };
