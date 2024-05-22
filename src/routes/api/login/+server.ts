16cac1f875665aa5bff1f956b9de003ff5eb5008
Glenn Della Rocca <glenn@dellarocca.mx>
2024-05-22 11:47:49 -0700

    feat: simple login handler

---
+++ src/routes/api/login/+server.ts
@@ -0,0 +1,12 @@
+import { randomUUID as uuid } from 'crypto';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }) => {
+	const data = await request.json();
+
+	if (!data.username) throw error(400, 'Username is required');
+	if (!data.password) throw error(400, 'Password is required');
+
+	return json({ name: data.username, id: uuid() });
+};
