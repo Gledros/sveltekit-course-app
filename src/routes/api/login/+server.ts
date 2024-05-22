de1be8c6351a9964341b3db1e2800451dd3b7438
Glenn Della Rocca <glenn@dellarocca.mx>
2024-05-22 14:09:28 -0700

    feat: set cookies when logged in

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -2,11 +2,14 @@ import { randomUUID as uuid } from 'crypto';
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async ({ request }) => {
+export const POST: RequestHandler = async ({ request, cookies }) => {
 	const data = await request.json();
 
 	if (!data.username) throw error(400, 'Username is required');
 	if (!data.password) throw error(400, 'Password is required');
 
-	return json({ name: data.username, id: uuid() });
+	const id = uuid();
+	cookies.set('token', id, { path: '/' });
+
+	return json({ name: data.username, token: id });
 };
